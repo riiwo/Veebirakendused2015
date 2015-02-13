413dcb95202d7c807d8ae2588b04448904d457b2
juksar <marcusjuksar@gmail.com>
2015-02-13 16:27:18 +0200

    Prototüübi failid

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head>
+<title>E-valimised</title>
+<meta http-equiv="description" content="page description" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<style type="text/css">@import "styles.css";</style>
+</head>
+
+<body>
+<div id="page">
+<div class="header">
+<div class="header-logo">
+<img src="./images/header-logo.jpg" alt="Taavi Rõivas">
+</div>
+<div id="menu">
+<ul>
+   <li class='active'><a href='#'>Home</a></li>
+   <li><a href='#'>Products</a></li>
+   <li><a href='#'>Company</a></li>
+   <li><a href='#'>Contact</a></li>
+</ul>
+</div>
+</div>
+</div>
+
+</body>
+</html>
---
+++ navigation.html
@@ -0,0 +1,8 @@
+<div id='cssmenu'>
+<ul>
+   <li class='active'><a href='#'>Home</a></li>
+   <li><a href='#'>Products</a></li>
+   <li><a href='#'>Company</a></li>
+   <li><a href='#'>Contact</a></li>
+</ul>
+</div>
