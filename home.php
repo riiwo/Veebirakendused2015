7a46d81e63602dbfdaa9ac236e47c924233efdab
Riivo Parts <riivoparts@gmail.com>
2015-02-13 18:35:48 +0200

    home uuendus

--- home.php
+++ home.php
@@ -1,4 +1,11 @@
 <?php include 'header.php';?>
-<div id="leftnav"></div>
+<div id="leftnav">
+    <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+    </ul>
+</div>
 <div id="main"></div>
 <?php include 'footer.php';?>
