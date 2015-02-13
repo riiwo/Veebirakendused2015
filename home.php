9060192fff759f5520375dfde27d3f24ce739f39
Riivo Parts <riivoparts@gmail.com>
2015-02-13 18:48:23 +0200

    teste
    test

--- home.php
+++ home.php
@@ -1,10 +1,10 @@
 <?php include 'header.php';?>
 <div id="leftnav">
     <ul>
-        <li><a href="#"></a></li>
-        <li><a href="#"></a></li>
-        <li><a href="#"></a></li>
-        <li><a href="#"></a></li>
+        <li><a href="#">KODU</a></li>
+        <li><a href="#">REDIGEERI</a></li>
+        <li><a href="#">STATISTIKA</a></li>
+        <li><a href="#">MIDAGI VEEL</a></li>
     </ul>
 </div>
 <div id="main"></div>
