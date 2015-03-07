5dac09874e7bc986edbedf4c382cb95bd0204046
Riivo Parts <riivoparts@gmail.com>
2015-03-07 20:19:27 +0200

    Failide uuenduse parandus

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include 'header.php';?>
-<div class="main">
+<div class="container">
             <div class="contentslider">
                 <h2>TULE VALIMA</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed iaculis tellus. Integer non luctus lectus, in faucibus ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum egestas mi orci, nec molestie justo interdum rhoncus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin eget leo volutpat, ullamcorper arcu vitae, pretium odio. Proin condimentum tellus ligula, at euismod tellus commodo ac.</p>
