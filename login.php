2c9641e7a64b40029607198f06b7d003f4c379d7
Riivo Parts <riivoparts@gmail.com>
2015-03-07 20:55:36 +0200

    Login parandus

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
         <input type="text">
         <p>Parool:</p>
         <input type="password">
-        <input type="POST" class="custombtn1" value="LOGI SISSE">
+        <input type="button" action="post"  class="custombtn1" value="LOGI SISSE">
     </form>  
 </div>
 <?php include 'footer.php';?>
