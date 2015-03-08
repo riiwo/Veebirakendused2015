df8d667809398d3a41ad05999b6fe2872deabcc3
Ohmoon <tsimmer9801@gmail.com>
2015-03-08 15:18:50 +0200

    Update login.php

--- login.php
+++ login.php
@@ -15,9 +15,7 @@ header("location: home.php");
         <label>Password :</label>
         <input id="password" name="password" type="password">
         <input name="submit" type="submit" value="Login ">
-        <a href='http://valimised.azurewebsites.net/logifb.php' style='text-decoration:none'>
-        <br><br>
-        <img src='images/fblogin.png' style='border:0;'></a>
+        
         <span><?php echo $error; ?></span>
     </form>
 </div>
