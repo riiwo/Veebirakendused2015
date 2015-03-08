9386bfe50b079f853aaca742800d23e6a7fafaf8
juksar <marcusjuksar@gmail.com>
2015-03-08 12:12:46 +0200

    andmebaasi connectioni test

---
+++ test_db_connection.php
@@ -0,0 +1,22 @@
+<?php
+
+// Database=valimisA1VJdQomV;
+// Data Source=eu-cdbr-azure-north-b.cloudapp.net;
+// User Id=b065e66df3ffd8;
+// Password=253cb2ae
+ 
+try {
+    $conn = new PDO ( "sqlsrv:server = tcp:eu-cdbr-azure-north-b.cloudapp.net; Database = valimisA1VJdQomV", "b065e66df3ffd8", "253cb2ae");
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch ( PDOException $e ) {
+    print( "Error connecting to SQL Server." );
+    die(print_r($e));
+}
+ 
+// SQL Server Extension Sample Code:
+ 
+$connectionInfo = array("UID" => "valimisA1VJdQomV", "pwd" => "253cb2ae", "Database" => "valimisA1VJdQomV", "LoginTimeout" => 30, "Encrypt" => 1);
+$serverName = "eu-cdbr-azure-north-b.cloudapp.net";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
