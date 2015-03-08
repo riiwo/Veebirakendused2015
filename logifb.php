46df111761b317bfd942d59510e551d2c2b4fcad
Ohmoon <tsimmer9801@gmail.com>
2015-03-08 15:15:09 +0200

    Create logifb.php

---
+++ logifb.php
@@ -0,0 +1,79 @@
+<?php
+	require_once( 'lib/Facebook/FacebookSession.php');
+	require_once( 'lib/Facebook/FacebookRequest.php' );
+	require_once( 'lib/Facebook/FacebookResponse.php' );
+	require_once( 'lib/Facebook/FacebookSDKException.php' );
+	require_once( 'lib/Facebook/FacebookRequestException.php' );
+	require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php');
+	require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
+	require_once( 'lib/Facebook/GraphObject.php' );
+	require_once( 'lib/Facebook/GraphUser.php' );
+	require_once( 'lib/Facebook/GraphSessionInfo.php' );
+	require_once( 'lib/Facebook/Entities/AccessToken.php');
+	require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
+	require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php');
+	require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
+
+
+	
+	use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+	use Facebook\FacebookResponse;
+	use Facebook\FacebookSDKException;
+	use Facebook\FacebookRequestException;
+	use Facebook\FacebookAuthorizationException;
+	use Facebook\GraphObject;
+	use Facebook\GraphUser;
+	use Facebook\GraphSessionInfo;
+	use Facebook\FacebookHttpable;
+	use Facebook\FacebookCurlHttpClient;
+	use Facebook\FacebookCurl;
+
+
+	 session_start();
+
+	
+	$app_id = '330684927125227';
+	$app_secret = '9bf364cb69dcadf390247bcbef41dc70';
+	$redirect_url='http://valimised.azurewebsites.net/';
+
+	
+	 FacebookSession::setDefaultApplication($app_id,$app_secret);
+	 $helper = new FacebookRedirectLoginHelper($redirect_url);
+	 $sess = $helper->getSessionFromRedirect();
+
+	
+	if(isset($_SESSION['fb_token'])){
+	 	$sess = new FacebookSession($_SESSION['fb_token']);
+	}
+
+	
+	
+
+	 
+	 	if(isset($sess)){
+	 		$_SESSION['fb_token']=$sess->getToken();
+	 		$request = new FacebookRequest($sess,'GET','/me');
+			$response = $request->execute();
+			$graph = $response->getGraphObject(GraphUser::classname());
+			$name = $graph->getName();
+			$id = $graph->getId();
+			$email = $graph->getProperty('email');
+			//echo "hi $name <br>"; andmed siit
+			
+	 	}else{
+	 		echo '<a href="'.$helper->getLoginUrl().'" >Login with facebook</a>';
+	 	}
+
+
+
+
+
+
+
+
+
+
+
+	
