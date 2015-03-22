16c899628b21e4ce99531b1fd34d91cc1f1db142
juksar <marcusjuksar@gmail.com>
2015-03-23 00:34:28 +0200

    Revert "test number 1"
    This reverts commit 6d62c25bddff4fa9ba33ebd08c048df10b60b83a.

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php include 'header.php';?>
 <div class="container">
             <div class="contentslider">
-                <h3>Tule valima</h3>
+                <h2>TULE VALIMA</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed iaculis tellus. Integer non luctus lectus, in faucibus ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum egestas mi orci, nec molestie justo interdum rhoncus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin eget leo volutpat, ullamcorper arcu vitae, pretium odio. Proin condimentum tellus ligula, at euismod tellus commodo ac.</p>
                 <a href=""><div class="custombtn1 left25">ANNA HÄÄL</div></a>
                 <a href=""><div class="custombtn1 right25">ESITA KANDIDATUUR</div></a>
             </div>
-            <div class="result">
-              <div class="result-text">
-                <h3>Hetke tulemused</h3>
+            <div class="resultslider">
+                <h2>HETKE TULEMUSED</h2>
                 <p>Curabitur ut cursus nisl. Cras nec dignissim lorem. Curabitur auctor, leo eget pharetra placerat, velit mi tristique diam, vitae aliquam arcu orci nec odio. Mauris blandit dictum condimentum. Proin a arcu viverra, lacinia odio a, dignissim tortor. Fusce vel porttitor ipsum. Etiam quis diam nec orci sollicitudin scelerisque eu porttitor libero.</p>
-                </div>
-                <div class="result-charts">
-                <div id= "result-chart-left">
+                <div class="resultchart">
                     <h4>Populaarsemad erakonnad</h4>
                     
                     <div id="chart_div"></div>
@@ -45,8 +42,9 @@
 
                         // Set chart options
                         var options = {'title':'',
+                                       'backgroundColor': '#c7e8e1',
                                        'width':"100%",
-                                       'height':200};
+                                       'height':150};
 
                         // Instantiate and draw our chart, passing in some options.
                         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
@@ -54,9 +52,9 @@
                       }
                       </script>
                 </div>
-                <div id="result-chart-middle">
-                    <h4>Populaarsemad kandidaadid</h4>
-                    <table id="result-chart-table">
+                <div class="resultchart">
+                    <h4>Populaarsemad inimesed</h4>
+                    <table>
                        <tbody>
                             <tr>
                                 <td><b>Edgar Rõiv</b></td>
@@ -81,12 +79,11 @@
                         </tbody>
                     </table>
                 </div>
-                <div id="result-chart-right">
+                <div class="resultchart">
                     <h4>Hetkel hääletanud</h4>
                     <p class="protsent">54%</p>
-                    <p>Valijaskonnast.</p>
+                    <p>Valisjaskonnast.</p>
                 </div>
-                </div>
-               </div>
+            </div>
         </div>
 <?php include 'footer.php';?>
--- sign_in.php
+++ sign_in.php
@@ -1,30 +1,40 @@
 <?php
+include('db/dbConfig.php');
+$dbConnected = mysqli_connect($server,$username,$password);
+$dbSelected = mysqli_select_db($dbConnected,$databaseName);
+//kas andmebaasiga saab uhenduse?
 
-include "functions.php";
+if($dbConnected){
+	//echo "mysql uhendatud";
+	if($dbSelected){
+		//echo "db connected";
+	} else {
+		//echo "db connection faield";
+	}
+} else {
+	echo "mysql connection faield";
+}
 
-$dbConnect = connectDb();
-$dbSelected = mysqli_select_db($dbConnect,'valimised');
-
+session_start();
+$error ="";
 if(isset($_POST['submit'])){
 	if(empty($_POST['email']) || empty($_POST['password'])){
-		echo "Emaili või parooli väli on tühi.";
+		$error="E-mail voi parool on vale";
 	}
 	else
 	{
 		$email = $_POST['email'];
 		$password = $_POST['password'];
-		$query = mysqli_query($dbConnect,"select * from user where password='$password'
+		$query = mysqli_query($dbConnected,"select * from user where password='$password'
 			and email='$email'");
-
 		$rows = mysqli_num_rows($query);
 		if($rows == 1){
-			$_SESSION['email']=$email;
-			$_SESSION['login'] = true;
+			$_SESSION['login_user']=$email;
 			header("location:home.php");
 		} else {
-			echo "Sisetatud parool või email on vale";
+			$error = "Email voi parool vale";
 		}
-		mysqli_close($dbConnect);
+		mysqli_close($dbConnected);
 	}
 }
 ?>
