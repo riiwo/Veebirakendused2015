e860dba8b61101a0b92a503d9712b548aa5bc52b
juksar <marcusjuksar@gmail.com>
2015-03-23 09:17:56 +0200

    menuu 2l tasemel

--- header.php
+++ header.php
@@ -20,9 +20,9 @@
             if(isset($_SESSION['login'])){
               echo '<li><a href="index.php">Kodu</a></li>';
               echo '<li><a href="Eesti_kaart.php">Statistika</a></li>';
+              echo '<li><a href="faq.php">KKK</a></li>';
               echo ' <li><a href="logout.php">Logi välja</a></li>';
             } else {
-              echo '<li><a href="index.php">Kodu</a></li>';
               echo '<li><a href="Eesti_kaart.php">Statistika</a></li>';
               echo '<li><a href="faq.php">KKK</a></li>';
               echo '<li><a href="login.php">Logi sisse</a></li>';
