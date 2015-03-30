8152e64671d801768fdbed9e0e75571d0df5359f
juksar <marcusjuksar@gmail.com>
2015-03-30 23:13:11 +0300

    init.php

--- core/init.php
+++ core/init.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include '../functions/functions.php';
-if(logged_in() === true){
+if(isset($_SESSION['user_id'])){
 	$session_id = $_SESSION['user_id'];
 	echo $session_id;
 }
