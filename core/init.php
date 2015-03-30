1f582d58f59ca3a171faa8ee64c17bd19bde064a
juksar <marcusjuksar@gmail.com>
2015-03-30 23:20:09 +0300

    Revert "katse"
    This reverts commit b86a15176c7f30c7c917791c4d12773ae4fa9dd9.

--- core/init.php
+++ core/init.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
 include '../functions/functions.php';
-
-if(isset($_SESSION['user_id'])){
-	$session_id = $_SESSION['user_id'];
-	echo $session_id;
-}
+// if(isset($_SESSION['user_id'])){
+// 	$session_id = $_SESSION['user_id'];
+// 	echo $session_id;
+// }
 
 ?>
