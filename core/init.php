7c36905127d80ec089996f53d7586993dbc40f56
juksar <marcusjuksar@gmail.com>
2015-03-30 22:16:39 +0300

    debug
    eelmise commiti debug katse.

--- core/init.php
+++ core/init.php
@@ -2,8 +2,8 @@
 session_start();
 include 'D:/home/site/wwwroot/functions/functions.php';
 
-if (logged_in()===true){
-	$session_id = $_SESSION['user_id'];
-	$user_data = user_data($session_id);
-}
+// if (logged_in()===true){
+// 	$session_id = $_SESSION['user_id'];
+// 	$user_data = user_data($session_id);
+// }
 ?>
