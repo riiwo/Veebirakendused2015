5d0e700e087a4a765fad7535fca1407432b1e0d2
juksar <marcusjuksar@gmail.com>
2015-03-30 22:20:54 +0300

    debug

--- core/init.php
+++ core/init.php
@@ -4,6 +4,6 @@ include '../functions/functions.php';
 
 if (logged_in()===true){
 	$session_id = $_SESSION['user_id'];
-	$user_data = user_data($session_id);
+	// $user_data = user_data($session_id);
 }
 ?>
