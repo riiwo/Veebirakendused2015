35cbd73a384f8875266ab58f35f498b73f7178ef
juksar <marcusjuksar@gmail.com>
2015-03-30 23:26:07 +0300

    Revert "katse"
    This reverts commit dd5bb3dd62050c62c0522dc8eb8e2126352122ce.

--- functions/login.php
+++ functions/login.php
@@ -1,29 +1,24 @@
 <?php
+include 'D:/home/site/wwwroot/core/init.php';
 
-if(logged_in()===true){
-	header("location:../home.php");
-} else {
-	include 'D:/home/site/wwwroot/core/init.php';
-
-	if(!empty($_POST)){
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-		if(empty($email) || empty($password)){
-			echo "Emaili voi parooli vÄli on tühi ";
-		}
+if(!empty($_POST)){
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	if(empty($email) || empty($password)){
+		echo "Emaili voi parooli vÄli on tühi ";
+	}
 	// else if(!user_exists($email)){
 	// 	echo "antud kasutajat pole olemas ";
 	// }
-		else {
-			$user_login = user_login($email, $password);
-			if(!$user_login){
-				echo "parool voi email vale";
-			} else {
-				$_SESSION['user_id'] = $user_login;
-				$_SESSION['email'] = $email;
-				header("location:../index.php");
-				exit();
-			}
+	else {
+		$user_login = user_login($email, $password);
+		if(!$user_login){
+			echo "parool voi email vale";
+		} else {
+			$_SESSION['user_id'] = $user_login;
+			$_SESSION['email'] = $email;
+			header("location:../index.php");
+			exit();
 		}
 	}
 }
