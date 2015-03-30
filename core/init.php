b86a15176c7f30c7c917791c4d12773ae4fa9dd9
juksar <marcusjuksar@gmail.com>
2015-03-30 23:19:28 +0300

    katse

--- core/init.php
+++ core/init.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 include '../functions/functions.php';
-// if(isset($_SESSION['user_id'])){
-// 	$session_id = $_SESSION['user_id'];
-// 	echo $session_id;
-// }
+
+if(isset($_SESSION['user_id'])){
+	$session_id = $_SESSION['user_id'];
+	echo $session_id;
+}
 
 ?>
