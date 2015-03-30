acb19b1acf5bd9d469ddde3f4f1fc6d59f621ea1
juksar <marcusjuksar@gmail.com>
2015-03-30 22:46:13 +0300

    debug

--- core/init.php
+++ core/init.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
-include '../functions/functions.php';
+include '/functions/functions.php';
 
-if (logged_in()){
-	echo "tootab";
-	$session_id = $_SESSION['user_id'];
-	echo $session_id;
-// $user_data = user_data($session_id);
-// echo $user_data;
-}
-echo "pole sessiooni";
+// if (logged_in()){
+// 	echo "tootab";
+// 	$session_id = $_SESSION['user_id'];
+// 	echo $session_id;
+// 	$user_data = user_data($session_id);
+// 	echo $user_data;
+// }
+// echo "pole sessiooni";
 ?>
