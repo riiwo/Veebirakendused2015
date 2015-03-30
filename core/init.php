2978faac9790aa8c748c47e94187f9e89e3d3a61
juksar <marcusjuksar@gmail.com>
2015-03-30 22:23:14 +0300

    debug

--- core/init.php
+++ core/init.php
@@ -4,7 +4,9 @@ include '../functions/functions.php';
 
 if (logged_in()){
 	echo "tootab";
-	// $session_id = $_SESSION['user_id'];
-	// $user_data = user_data($session_id);
+	$session_id = $_SESSION['user_id'];
+	echo $session_id;
+	$user_data = user_data($session_id);
+	echo $user_data;
 }
 ?>
