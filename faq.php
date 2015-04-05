c1382d59bed4358fb0a50e5333730779a8a4f3c1
Ohmoon <tsimmer9801@gmail.com>
2015-04-05 21:11:20 +0300

    Update faq.php

--- faq.php
+++ faq.php
@@ -8,43 +8,43 @@
           </div>
       </div>
       <div class="question-container">
-          <label class="question" for="answer-2"><h4>Küsimus 2</h4></label>
-          <input type="checkbox" id="answer-2">
+          <h4><label class="question" for="answer-2">Küsimus 2</label></h4>
+          <input type="checkbox" id="answer-2"/>
           <div class="answer">
               <p>Lorem consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta cursus lectus. Proin nunc erat, gravida a facilisis quis, ornare id lectus. Proin consectetur nibh quis.</p>
           </div>
       </div>
       <div class="question-container">
-          <label class="question" for="answer-3"><h4>Küsimus 3</h4></label>
-          <input type="checkbox" id="answer-3">
+          <h4><label class="question" for="answer-3">Küsimus 3</label></h4>
+          <input type="checkbox" id="answer-3"/>
           <div class="answer">
               <p>Lorem consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta cursus lectus. Proin nunc erat, gravida a facilisis quis, ornare id lectus. Proin consectetur nibh quis.</p>
           </div>
       </div>
       <div class="question-container">
-          <label class="question" for="answer-4"><h4>Küsimus 4</h4></label>
-          <input type="checkbox" id="answer-4">
+          <h4><label class="question" for="answer-4">Küsimus 4</label></h4>
+          <input type="checkbox" id="answer-4"/>
           <div class="answer">
               <p>Lorem consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta cursus lectus. Proin nunc erat, gravida a facilisis quis, ornare id lectus. Proin consectetur nibh quis.</p>
           </div>
       </div>
       <div class="question-container">
-          <label class="question" for="answer-5"><h4>Küsimus 5</h4></label>
+          <h4><label class="question" for="answer-5">Küsimus 5</label></h4>
           <input type="checkbox" id="answer-5">
           <div class="answer">
               <p>Lorem consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta cursus lectus. Proin nunc erat, gravida a facilisis quis, ornare id lectus. Proin consectetur nibh quis.</p>
           </div>
       </div>
       <div class="question-container">
-          <label class="question" for="answer-6"><h4>Küsimus 6</h4></label>
-          <input type="checkbox" id="answer-6">
+          <h4><label class="question" for="answer-6">Küsimus 6</label></h4>
+          <input type="checkbox" id="answer-6"/>
           <div class="answer">
               <p>Lorem consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta cursus lectus. Proin nunc erat, gravida a facilisis quis, ornare id lectus. Proin consectetur nibh quis.</p>
           </div>
       </div>
       <div class="question-container">
-          <label class="question" for="answer-7"><h4>Küsimus 7</h4></label>
-          <input type="checkbox" id="answer-7">
+          <h4><label class="question" for="answer-7">Küsimus 7</label></h4>
+          <input type="checkbox" id="answer-7"/>
           <div class="answer">
               <p>Lorem consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta cursus lectus. Proin nunc erat, gravida a facilisis quis, ornare id lectus. Proin consectetur nibh quis.</p>
           </div>
