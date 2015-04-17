fb453667b92604d518530a6368905233482028ad
Ohmoon <tsimmer9801@gmail.com>
2015-04-17 20:15:54 +0300

    Create head_index.php

---
+++ includes/head_index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" manifest= 'valimised.manifest' > 
+
+
+<head>
+<title>E-valimised</title>
+<meta http-equiv="description" content="page description" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<style type="text/css">@import "css/styles.css";</style>
+<style type="text/css">@import "css/modal.css";</style>
+<style type="text/css">@import "css/normalize.css";</style>
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/jquery.imagemapster.min.js"></script>
+<script type="text/javascript" src="js/responsive_maps.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+<script type="text/javascript" src="js/register.js"></script>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
