2830de912e6720e56382e95ec3e2ce661f5977a6
Riivo Parts <riivoparts@gmail.com>
2015-04-27 00:34:05 +0300

    töötav DataPush

--- core/getData.php
+++ core/getData.php
@@ -1,8 +1,8 @@
 <?php include_once 'init.php'; ?>
 <?php
     $connection = dbConnect();
-    $result = mysqli_fetch_assoc(mysqli_query($connection, "SELECT COUNT(*) AS number FROM haaletustulemus"));
+    $result = mysqli_query($connection, "SELECT COUNT(*) FROM haaletustulemus");
     $row = mysqli_fetch_array($result);
-    $thing = $row['number'];
-    return $thing;
+    $thing = $row[0];
+    echo $thing;
 ?>
