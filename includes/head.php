7b893bf74f106a4a9a7a1fa7e55fc041362cb975
juksar <marcusjuksar@gmail.com>
2015-04-27 12:19:49 +0300

    head.php

--- includes/head.php
+++ includes/head.php
@@ -3,11 +3,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" > 
 <head>
 <title>E-valimised</title>
-<meta http-equiv="description" content="page description" />
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="./css/styles.css">
-<link rel="stylesheet" type="text/css" href="./css/normalize.css">
-<link rel="stylesheet" type="text/css" href="./css/modal.css">
+<meta http-equiv="description" content="page description">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" type="text/css" href="css/normalize.css">
+<link rel="stylesheet" type="text/css" href="css/modal.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
 <script type="text/javascript" src="js/jquery.imagemapster.min.js"></script>
 <script type="text/javascript" src="js/responsive_maps.js"></script>
@@ -15,5 +16,4 @@
 <script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
 <script type="text/javascript" src="js/register.js"></script>
 <script type="text/javascript" src="js/login.js"> </script>
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
