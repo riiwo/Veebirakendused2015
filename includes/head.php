a64c6634670547b4341c95641be02aa19d75650d
juksar <marcusjuksar@gmail.com>
2015-04-27 12:12:53 +0300

    head.php
    @import aeglustab

--- includes/head.php
+++ includes/head.php
@@ -5,9 +5,9 @@
 <title>E-valimised</title>
 <meta http-equiv="description" content="page description" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<style type="text/css">@import "css/styles.css";</style>
-<style type="text/css">@import "css/modal.css";</style>
-<style type="text/css">@import "css/normalize.css";</style>
+<link rel="stylesheet" type="text/css" href="./css/styles.css">
+<link rel="stylesheet" type="text/css" href="./css/normalize.css">
+<link rel="stylesheet" type="text/css" href="./css/modal.css">
 <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
 <script type="text/javascript" src="js/jquery.imagemapster.min.js"></script>
 <script type="text/javascript" src="js/responsive_maps.js"></script>
