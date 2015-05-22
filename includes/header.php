851852480402a157f2225055b6f4326295194283
juksar <marcusjuksar@gmail.com>
2015-05-22 11:07:20 +0300

    menüü

--- includes/header.php
+++ includes/header.php
@@ -1,31 +1,29 @@
 <?php
 if (basename($_SERVER['PHP_SELF'])=='Eesti_kaart.php') {
-    include 'includes/head_index.php';
+  include 'includes/head_index.php';
 } else {
-    include 'includes/head.php';
+  include 'includes/head.php';
 }
 include './core/init.php';   
 ?>
 <body>
   <div id="page">
-<div class="header">
     <div class="row">
-    <div class="large-8 medium-6 small-12 column">
-     <input type="hidden" id="help" value="index.php">
-      <a href="index.php"><img src="./images/logo2.png" alt="home" /></a>
-    </div>
-    <div class="large-4 medium-6 small-12 column">
-        <ul class="main-menu">
+      <div class="large-8 medium-6 small-12 column">
+       <input type="hidden" id="help" value="index.php">
+       <a href="index.php"><img src="./images/logo2.png" alt="home" /></a>
+     </div>
+     <div class="large-4 medium-6 small-12 column">
+      <ul>
         <?php if(logged_in()){
-            echo '<li><a href="home.php">Kodu</a></li>';
-            echo '<li><a href="logout.php">Logi välja</a></li>';
+          echo '<li><a href="home.php">Kodu</a></li>';
+          echo '<li><a href="logout.php">Logi välja</a></li>';
         } else {
-            echo '<li><a href="Eesti_kaart.php">Statistika</a></li>';
-            echo '<li><a href="faq.php">KKK</a></li>';
-            echo '<li><a class="modal_trigger" href="#modal">Sisene</a></li>';
+          echo '<li><a href="Eesti_kaart.php">Statistika</a></li>';
+          echo '<li><a href="faq.php">KKK</a></li>';
+          echo '<li><a class="modal_trigger" href="#modal">Sisene</a></li>';
         }
         ?>
-        </ul>
-        </div>
-</div>
-      </div>
+      </ul>
+    </div>
+  </div>
