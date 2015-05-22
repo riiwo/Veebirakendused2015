362a229c1c9a96fa4664c1dede0ea695ec453f93
juksar <marcusjuksar@gmail.com>
2015-05-22 11:09:11 +0300

    menüü

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@ include './core/init.php';
 ?>
 <body>
   <div id="page">
-    <div class="row">
+  <div class="row" id="header">
       <div class="large-8 medium-6 small-12 column">
        <input type="hidden" id="help" value="index.php">
        <a href="index.php"><img src="./images/logo2.png" alt="home" /></a>
