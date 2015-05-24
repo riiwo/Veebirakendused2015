ccb9f2a9df69f9346d3810936b31c7fd88851956
juksar <marcusjuksar@gmail.com>
2015-05-24 18:56:37 +0300

    head tühjendus

--- includes/head.php
+++ includes/head.php
@@ -8,7 +8,6 @@
 <link rel="stylesheet" type="text/css" href="css/modal.css">
 <link rel="stylesheet" type="text/css" href="css/foundation.css">
 <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
-<script type="text/javascript" src="js/jquery.imagemapster.min.js"></script>
 <script type="text/javascript" src="js/responsive_maps.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
 <script type="text/javascript" src="js/test.js"></script>
