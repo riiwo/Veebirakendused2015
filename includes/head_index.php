cd5eaa0a6fdfed4ae3d6cbfebf88cc894f6eaade
juksar <marcusjuksar@gmail.com>
2015-05-24 21:16:31 +0300

    test

--- includes/head_index.php
+++ includes/head_index.php
@@ -11,7 +11,8 @@
 <script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
 <script type="text/javascript" src="js/register.js"></script>
 <script type="text/javascript" src="js/test.js"></script>
-<script type="text/javascript" src="js/foundation.min.js"></script>
 <script type="text/javascript" src="js/modernizr.js"></script>
 <script type="text/javascript" src="js/fastclick.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/foundation.min.js"></script>
 </head>
