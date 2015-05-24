c27ab156ade4bd94ee21313e2570d386dd65731c
juksar <marcusjuksar@gmail.com>
2015-05-24 23:14:03 +0300

    test

--- statistika.php
+++ statistika.php
@@ -42,6 +42,7 @@
     </table>
   </div>
   <div class="small-12 medium-6 large-3">
+  <form method="get" action="filter.php">
     <select name="ringkond">
                 <option value="">Vali ringkond...</option>
                 <?php 
@@ -53,8 +54,31 @@
                     } else {
                         echo '<option value="'.$row['PiirkondID'].'">'.$row['Piirkond'].'</option>';
                     }
-                }?>
+                }
+
+                ?>
             </select>
+            <input type ="submit" value="Tulemused">
+        </form>
+        <table>
+      <tbody>
+        <tr>
+          <th colspan="2">Nimi</th>
+          <th>Erakond</th>
+          <th>Tulemus</th>
+        </tr>
+        <?php
+        while($row = mysqli_fetch_array($data)){
+          echo "<tr>";
+          echo "<td>" . $row['firstname'] . "</td>";
+          echo "<td>" . $row['lastname'] . "</td>";
+          echo "<td>" . $row['nimi'] . "</td>";
+          echo "<td>" . $row['votes'] . "</td>";
+          echo "</tr>";
+        }
+        ?>
+      </tbody>
+    </table>
   </div>
   <div class="small-12 medium-6 large-3">
 
