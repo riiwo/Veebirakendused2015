3726082ff631400c037ff126e741b4d1ed037214
juksar <marcusjuksar@gmail.com>
2015-05-25 00:19:53 +0300

    Revert "test"
    This reverts commit 34b326728ebd39a18cd209e870ee6ff6f24d699c.

--- statistika.php
+++ statistika.php
@@ -61,27 +61,6 @@
         <div id="filter_result">
 
         </div>
-        <script type="text/javascript">
-        function filter_ringkond(str){
-  if(str==""){
-    document.getElementById("filter_result").innerHTML = "";
-    return;
-  } else {
-    if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                document.getElementById("filter_result").innerHTML = xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET","filter_ringkond.php?q="+str,true);
-  }
-}</script>
   </div>
   <div class="small-12 medium-6 large-3">
 
