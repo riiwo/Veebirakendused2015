9a3fec3d45aee613cf845335605ec2697e7ee1ab
juksar <marcusjuksar@gmail.com>
2015-05-25 07:09:58 +0300

    test

--- filter_ringkond.php
+++ filter_ringkond.php
@@ -7,7 +7,7 @@ $sql = ("SELECT users.firstname,users.lastname,erakond.nimi,COUNT(haaletustulemu
 		JOIN erakond on kandidaat.erakondid = erakond.id
         JOIN ringkond on kandidaat.ringkondid = ringkond.PiirkondID
         JOIN users on kandidaat.userid = users.user_id
-        WHERE ringkond.Piirkond = '$q'
+        WHERE ringkond.PiirkondID = '$q'
 		GROUP BY erakond.nimi");
 echo "$q";
 
